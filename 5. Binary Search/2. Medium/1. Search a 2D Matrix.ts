61c7523652c9c63d0c4fb3cb831dec44ed3b5316
David Chen <DavidChen@Davids-iMac.local>
2023-04-03 16:22:43 -0400

    added answer

--- 5. Binary Search/2. Medium/1. Search a 2D Matrix.ts
+++ 5. Binary Search/2. Medium/1. Search a 2D Matrix.ts
@@ -20,8 +20,17 @@
 
 
 var searchMatrix = function(matrix:number[][], target:number) {
-    
+    for(let ele of matrix){
+        if(ele[0]<=target&&ele[ele.length-1]>=target){
+            for(let i=0;i<ele.length;i++){
+                if(ele[i]==target){
+                    return true
+                }
+            }
+        }
+    }
+    return false
 };
 
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)) // True
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)) // False
