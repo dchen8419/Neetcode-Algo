544f4ac3ca0e81ad7be72c3645f16757d713becd
David Chen <DavidChen@Davids-iMac.local>
2023-04-04 15:50:10 -0400

    added answer

--- 5. Binary Search/2. Medium/2. Koko Eating Bananas.ts
+++ 5. Binary Search/2. Medium/2. Koko Eating Bananas.ts
@@ -21,7 +21,29 @@
 // Output: 23
 
 var minEatingSpeed = function(piles: number[], h:number) {
-    
+     // Start at an eating speed of 1.
+    let speed = 1;
+
+    while (true) {
+         // hourSpent stands for the total hour Koko spends with 
+         // the given eating speed.
+        let hourSpent = 0;
+
+         // Iterate over the piles and calculate hourSpent.
+         // We increase the hourSpent by ceil(pile / speed)
+        for (const pile of piles) {
+            hourSpent += Math.ceil(pile / speed);
+        }        
+        
+         // Check if Koko can finish all the piles within h hours,
+         // If so, return speed. Otherwise, let speed increment by
+         // 1 and repeat the previous iteration.
+        if (hourSpent <= h) {
+            return speed;
+        } else {
+            speed += 1;
+        }
+    }
 };
 
 console.log(minEatingSpeed([3,6,7,11], 8)) //4
