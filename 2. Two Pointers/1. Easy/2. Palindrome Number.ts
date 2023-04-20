aebe1007f4f2179b1775c3066377360010f5cd2d
David Chen <DavidChen@Davids-iMac.local>
2023-04-20 12:30:26 -0400

    added answer to palindrome number

--- 2. Two Pointers/1. Easy/2. Palindrome Number.ts
+++ 2. Two Pointers/1. Easy/2. Palindrome Number.ts
@@ -19,9 +19,20 @@
 // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
 var isPalindrome = function(x: number) {
-    
+    var reverse: number = 0;
+    var copy: number = x;
+
+    //The loop break when the copy of original number becomes zero
+    //Also negative number cannot be a palindrome
+    while (copy > 0) {
+        const digit: number = copy % 10;
+        reverse = reverse * 10 + digit;
+        copy = ~~(copy / 10);
+    }
+
+    return reverse == x;
 };
 
-console.log(isPalindrome(121))
-console.log(isPalindrome(-121))
-console.log(isPalindrome(10))
+console.log(isPalindrome(121)) // true
+console.log(isPalindrome(-121)) // false
+console.log(isPalindrome(10)) // false
