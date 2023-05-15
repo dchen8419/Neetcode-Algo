63f519d3191bd20c30430b9007efedd1307dc7f8
David Chen <DavidChen@Davids-iMac.local>
2023-05-15 12:25:25 -0400

    added merge sort answer

--- 1. Arrays & Hashing/2. Medium/8. Sort Colors.ts
+++ 1. Arrays & Hashing/2. Medium/8. Sort Colors.ts
@@ -16,7 +16,29 @@
 // Output: [0,1,2]
 
 var sortColors = function(nums: number[]) {
-    
+    if (nums.length < 2) return nums;
+    let mid = Math.floor(nums.length/2);
+    let left: number[] = nums.slice(0, mid);
+    let right: number[] = nums.slice(mid);
+
+    function merging(left: number[], right: number[]) {
+        let result: number[] = [];
+        let lLen = left.length;
+        let rLen = right.length;
+        let l: number = 0;
+        let r: number = 0;
+        while(l < lLen && r < rLen){
+            if(left[l] < right[r]){
+                result.push(left[l++]);
+            }
+            else{
+                result.push(right[r++]);
+            }
+        }  
+        return result.concat(left.slice(l)).concat(right.slice(r));
+    }
+
+    return merging(sortColors(left),sortColors(right));
 };
 
 console.log(sortColors([2,0,2,1,1,0])) // [0,0,1,1,2,2]
