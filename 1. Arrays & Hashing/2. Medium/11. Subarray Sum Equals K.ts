2461c23894992be77093246399228d0e54444e57
David Chen <DavidChen@Davids-iMac.local>
2023-05-19 12:14:57 -0400

    added answer

--- 1. Arrays & Hashing/2. Medium/11. Subarray Sum Equals K.ts
+++ 1. Arrays & Hashing/2. Medium/11. Subarray Sum Equals K.ts
@@ -12,9 +12,33 @@
 // Input: nums = [1,2,3], k = 3
 // Output: 2
 
+/*Brute Foce*/
 var subarraySum = function(nums: number[], k: number) {
-    
+    let counter = 0;
+    for (let i=0;i<nums.length;i++) {
+        let base = 0;
+        for (let j=i;j<nums.length;j++) {
+            base+=nums[j];
+            if (base == k) counter++;
+        }
+    }
+    return counter;
 };
+
+/*Hash Map*/
+var subarraySumHashMap = function(nums: number[], k: number) {
+    let map = new Map();
+    let sum = 0;
+    let count = 0;
+    map.set(0,1);
+    for (let i=0;i<nums.length;i++) {
+        sum += nums[i];
+        if (map.has(sum-k)) count += map.get(sum-k);
+        if (map.has(sum)) map.set(sum, map.get(sum)+1);
+        else map.set(sum, 1);
+    }
+    return count;
+}
 
 console.log(subarraySum([1,1,1], 2))
 console.log(subarraySum([1,2,3], 3))
