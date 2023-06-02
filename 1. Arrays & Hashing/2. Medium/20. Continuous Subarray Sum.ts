48c4578b01d48c801b8c50ee66fa5b0b9ff62440
David Chen <DavidChen@Davids-iMac.local>
2023-06-02 10:08:05 -0400

    added console to debug

--- 1. Arrays & Hashing/2. Medium/20. Continuous Subarray Sum.ts
+++ 1. Arrays & Hashing/2. Medium/20. Continuous Subarray Sum.ts
@@ -28,3 +28,7 @@
 var checkSubarraySum = function(nums: number[], k: number) {
     
 };
+
+console.log(checkSubarraySum([23,2,4,6,7], 6)) // true
+console.log(checkSubarraySum([23,2,6,4,7], 6)) //true
+console.log(checkSubarraySum([23,2,6,4,7], 13)) //false
