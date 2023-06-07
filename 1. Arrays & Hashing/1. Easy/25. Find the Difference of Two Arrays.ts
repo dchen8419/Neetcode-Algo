212f701921c5e6ca72cb6ec6d79ea624c9217980
David Chen <DavidChen@Davids-iMac.local>
2023-06-07 09:52:50 -0400

    added first conditional

--- 1. Arrays & Hashing/1. Easy/25. Find the Difference of Two Arrays.ts
+++ 1. Arrays & Hashing/1. Easy/25. Find the Difference of Two Arrays.ts
@@ -27,7 +27,9 @@ var findDifference = function(nums1: number[], nums2: number[]) {
 
     for ( let i = 0; i < nums1.length; i ++ ) {
         for ( let j = 0; j < nums2.length; j ++ ) {
-            
+            if (nums1[i] !== nums2[j]) {
+                difNums1.push(nums1[i])
+            } 
         }
     }
 
