ad5b671250e8ee70518c151b33923a271e6d746e
David Chen <DavidChen@Davids-iMac.local>
2023-06-12 10:53:02 -0400

    added problem and console with data types

--- 1. Arrays & Hashing/3. Hard/1. First Missing Positive.ts
+++ 1. Arrays & Hashing/3. Hard/1. First Missing Positive.ts
@@ -17,3 +17,11 @@
 // Input: nums = [7,8,9,11,12]
 // Output: 1
 // Explanation: The smallest positive integer 1 is missing.
+
+var firstMissingPositive = function(nums: number[]) {
+    
+};
+
+console.log(firstMissingPositive([1,2,0])); // 3
+console.log(firstMissingPositive([3,4,-1,1])); // 2
+console.log(firstMissingPositive([7,8,9,11,12])); // 1
