bcebadd6eeba8d79ab24fefcb982e4dd365593e4
David Chen <DavidChen@Davids-iMac.local>
2023-06-13 08:55:10 -0400

    added forloop and short circuit

--- 1. Arrays & Hashing/2. Medium/21. Repeat DNA Sequences.ts
+++ 1. Arrays & Hashing/2. Medium/21. Repeat DNA Sequences.ts
@@ -19,6 +19,13 @@ var findRepeatedDnaSequences = function(s: string) {
     let curr = s.slice(0, 10);
     const seen = new Set([curr]);
     const res = new Set();
+
+    for(let i = 10; i < s.length; i++) {
+        curr = curr.slice(1) + s[i];
+        if(seen.has(curr)) res.add(curr);
+        seen.add(curr);
+    }
+    return [...res]
 };
 
 console.log(findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")) // ["AAAAACCCCC","CCCCCAAAAA"]
