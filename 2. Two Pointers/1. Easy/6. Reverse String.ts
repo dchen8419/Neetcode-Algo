27b08cdda6c02d7ff160adfeef0a48a0d804c5c9
David Chen <DavidChen@Davids-iMac.local>
2023-06-22 07:44:54 -0400

    added consoles

--- 2. Two Pointers/1. Easy/6. Reverse String.ts
+++ 2. Two Pointers/1. Easy/6. Reverse String.ts
@@ -14,3 +14,6 @@
 var reverseString = function(s: string[]) {
     
 };
+
+console.log(reverseString(["h","e","l","l","o"])) //["o","l","l","e","h"]
+console.log(reverseString(["H","a","n","n","a","h"])) //["h","a","n","n","a","H"]
