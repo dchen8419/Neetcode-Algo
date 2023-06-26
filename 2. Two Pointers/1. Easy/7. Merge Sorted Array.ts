e1939d968b8da7c8a479001ed4fbefe49a4e0cc2
David Chen <DavidChen@Davids-iMac.local>
2023-06-26 10:31:52 -0400

    added debug

--- 2. Two Pointers/1. Easy/7. Merge Sorted Array.ts
+++ 2. Two Pointers/1. Easy/7. Merge Sorted Array.ts
@@ -31,3 +31,6 @@ var merge = function(nums1: number[], m: number, nums2: number[], n: number) {
     
 };
 
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3)) //[1,2,2,3,5,6]
+console.log(merge([1],1,[],0)) //[1]
+console.log(merge([0], 0, [1], 1)) //[1]
