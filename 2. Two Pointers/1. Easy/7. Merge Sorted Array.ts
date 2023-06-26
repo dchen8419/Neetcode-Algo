ad6a00986f99a9f7eb047a93ce633404d59c50c1
David Chen <DavidChen@Davids-iMac.local>
2023-06-26 10:19:49 -0400

    added function with typing

--- 2. Two Pointers/1. Easy/7. Merge Sorted Array.ts
+++ 2. Two Pointers/1. Easy/7. Merge Sorted Array.ts
@@ -26,3 +26,8 @@
 // Explanation: The arrays we are merging are [] and [1].
 // The result of the merge is [1].
 // Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+var merge = function(nums1: number[], m: number, nums2: number[], n: number) {
+    
+};
+
