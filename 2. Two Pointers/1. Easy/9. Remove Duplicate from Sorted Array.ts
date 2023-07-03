fd1bf7e0a99068840c94fde7d996422c349073c6
David Chen <DavidChen@Davids-iMac.local>
2023-07-03 16:55:28 -0400

    added consoles

--- 2. Two Pointers/1. Easy/9. Remove Duplicate from Sorted Array.ts
+++ 2. Two Pointers/1. Easy/9. Remove Duplicate from Sorted Array.ts
@@ -35,3 +35,6 @@
 var removeDuplicates = function(nums: number[]) {
     
 };
+
+console.log(removeDuplicates([1,1,2])) //2, [1,2,_]
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])) //5, [0,1,2,3,4,_,_,_,_,_]
