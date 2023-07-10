2f290e72e5420dcd9847d8517e9d5327d5a7597c
David Chen <davidchen@Davids-MacBook-Pro.local>
2023-07-10 18:32:59 -0400

    added console numbers

--- 2. Two Pointers/2. Medium/5. 4Sum.ts
+++ 2. Two Pointers/2. Medium/5. 4Sum.ts
@@ -17,3 +17,6 @@
 var fourSum = function(nums: number[], target: number) {
     
 };
+
+console.log(fourSum([1,0,-1,0,-2,2], 0)) //[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+console.log(fourSum([2,2,2,2,2], 8)) //[[2,2,2,2]]
