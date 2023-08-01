87fcd0b4d802494d2f39207a7076ed55ec2503cd
David Chen <DavidChen@Davids-iMac.local>
2023-08-01 13:35:48 -0400

    added problem

--- 2. Two Pointers/2. Medium/5. 4Sum.ts
+++ 2. Two Pointers/2. Medium/5. 4Sum.ts
@@ -65,10 +65,10 @@ var fourSum = function(nums: number[], target: number) {
                     //if it doesn't match we will increment left by 1 and decrement right by 1 using a while loop
                     while (left < right && nums[left] === nums[left + 1]) {
                         left++;
-                      }
+                    }
                     while (left < right && nums[right] === nums[right - 1]) {
                         right--;
-                      }
+                    }
                     left++ ;
                     right-- ;
                 }   
@@ -76,7 +76,7 @@ var fourSum = function(nums: number[], target: number) {
         }
     }
          //return the numbers array
-         return quadruplets
+        return quadruplets
 };
 
 console.log(fourSum([1,0,-1,0,-2,2], 0)) //[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
