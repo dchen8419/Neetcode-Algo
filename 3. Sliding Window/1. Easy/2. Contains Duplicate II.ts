f816b6deeabe1ba58b529486c9a90646897c52a3
David Chen <DavidChen@Davids-iMac.local>
2023-08-16 07:35:00 -0400

    added conditional

--- 3. Sliding Window/1. Easy/2. Contains Duplicate II.ts
+++ 3. Sliding Window/1. Easy/2. Contains Duplicate II.ts
@@ -23,6 +23,9 @@ var containsNearbyDuplicate = function(nums: number[], k: number) {
     //create a for loop that loops through nums at each index
     for ( let i = 0; i < nums.length; i ++ ) {
         //create an if condition that if the index minus the hashmap at the nums index is less than or equal to k we will return true
+        if ( i - hashmap.get(nums[i] <= k) ) {
+            
+        }
         //for each loop we will setup the Map object as key being nums[i] and the value as i
     }
     //return false
