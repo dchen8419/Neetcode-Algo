637ec8cf560c7569c8e491411d4f2454faf95709
David Chen <DavidChen@Davids-iMac.local>
2023-08-21 12:57:23 -0400

    added windowSum and set to zero

--- 3. Sliding Window/2. Medium/5. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.ts
+++ 3. Sliding Window/2. Medium/5. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.ts
@@ -21,6 +21,7 @@ var numOfSubarrays = function(arr: number[], k: number, threshold: number) {
     //create variable for windowSize;
     let windowSize = 0;
     //create variable for windowSum;
+    let windowSum = 0;
     //create a for loop starting at 0 and increment one at a time from the nums.length;
         //if the windowSize variable is equal to the k number we will do another conditional
             //if the windowSize divided by k is less than the threshold we will increment count++, set windowSum -= to nums[i-k], and decrement windowSize--
