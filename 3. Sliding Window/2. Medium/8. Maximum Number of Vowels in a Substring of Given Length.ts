949eb423a0095a2c5cb3fd5f1a2040e3a686a61b
David Chen <DavidChen@Davids-iMac.local>
2023-09-26 10:47:20 -0400

    added max

--- 3. Sliding Window/2. Medium/8. Maximum Number of Vowels in a Substring of Given Length.ts
+++ 3. Sliding Window/2. Medium/8. Maximum Number of Vowels in a Substring of Given Length.ts
@@ -33,8 +33,10 @@ We attempt to return at multiple points in the code if max or current have hit t
 var maxVowels = function(s: string, k: number) {
     //create a set for vowels using const
     const vowels = new Set(['a','e','i','o','u','y'])
-    //create a max vaariable
+    //create a max variable 
+    let max = 0; //Max number of vowels in any window
     //create current variable
+    
     //create a for loop that will iterate thru i < k starting at i = 0 and incrementing one at a time
         //the condition is if the s[i] contains the vowels then we will increment max by 1
     //once the for loop is done we hit another condition outside of the for loop in the function global where if max is the same as k we will return the max that's when we know that k is the max we can go
