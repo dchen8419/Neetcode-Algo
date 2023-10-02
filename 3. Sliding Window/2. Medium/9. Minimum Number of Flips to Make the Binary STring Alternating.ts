aaf7e0e0f6c1f1fba9f716b49bf7b97410857ad4
David Chen <DavidChen@Davids-iMac.local>
2023-10-02 09:06:05 -0400

    added sudo code for oddCmp

--- 3. Sliding Window/2. Medium/9. Minimum Number of Flips to Make the Binary STring Alternating.ts
+++ 3. Sliding Window/2. Medium/9. Minimum Number of Flips to Make the Binary STring Alternating.ts
@@ -101,6 +101,7 @@ var minFlips = function(s:string) {
     //create a const where initalLen is equal to the initial s.length
     //create a condition if the initialLen is % 2 then we will make a copy of s and then attach it ot the end
     //create evenCmp variable set to 0
+    //create oddCmp set to 0
 };
 
 console.log(minFlips("111000")) //2
