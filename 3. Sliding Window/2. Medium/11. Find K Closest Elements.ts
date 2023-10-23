cf4e42785b595ab41662befd3ff8b98e2d6557b9
David Chen <DavidChen@Davids-iMac.local>
2023-10-23 12:34:01 -0400

    added ans

--- 3. Sliding Window/2. Medium/11. Find K Closest Elements.ts
+++ 3. Sliding Window/2. Medium/11. Find K Closest Elements.ts
@@ -17,13 +17,13 @@
 
 var findClosestElements = function(arr: number[], k: number, x: number) {
     //sort the arr and create two varaibles where a1 is the abs of a-x and b1 is the abs of b-x
-    
     arr.sort((a,b) => {
         const a1 = Math.abs(a-x), b1 = Math.abs(b-x);
         //the return should be if a1 === b1 then we return a-b but if it's false we return a1-b1
         return a1 == b1 ? a-b : a1-b1;
     });
     //we then create a varaible ans to be the sorted arr but slice at 0, k
+    const ans = arr.slice(0,k)
     //we then sort it again from a - b
     //finally we return the ans
 };
