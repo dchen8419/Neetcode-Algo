5f04e72c4ffbfdd3671837bcd8e69caae35f9bfe
David Chen <DavidChen@Davids-iMac.local>
2023-10-25 09:11:37 -0400

    changed sudo of pop

--- 4. Stack/1. Easy/3. Implement Stack using Queues.ts
+++ 4. Stack/1. Easy/3. Implement Stack using Queues.ts
@@ -38,7 +38,7 @@ MyStack.prototype.push = function(x: number) {
 };
 
 MyStack.prototype.pop = function() {
-    //pop off the last element in the stack which is using pop method but we need to check if the stack contains any elements
+    //remove the element on the top of the stack which pop method will do that but we need to check if the stack contains any elements
     if (this.stack.length === 0) {
         return null; //we can also throw an error because popping from an empty stack is undefined behavior
     }
